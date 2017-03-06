6f047205280629025e2c726f3c78657180bdc5c0
Angie <352444418@qq.com>
2017-03-06 11:28:08 +0800

    基础框架搭建完成。拿来直接使用即可。

---
+++ zhibo/Module/Home/Controller/HomeViewController.m
@@ -0,0 +1,40 @@
+//
+//  HomeViewController.m
+//  zhibo
+//
+//  Created by K.Lam on 2017/3/3.
+//  Copyright © 2017年 wbiao. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor orangeColor];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
